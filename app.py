9a754609bc57a64440a31f672b32cfea57e0bb76
dougaltoms <dougal.toms@gmail.com>
2024-09-07 10:40:29 +0100

    Add input widgets

--- app.py
+++ app.py
@@ -1,41 +1,64 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import extra_streamlit_components as stx
 
-st.header('Importing a pickled regression model')
-st.markdown('---')
+st.header('Rewilder')
+st.subheader('Mapping rewilding suitability for native ancient tree species')
 
-col1, col2, col3, col4, col5 =  st.columns(5)
+chosen_id = stx.tab_bar(data=[
+    stx.TabBarItemData(id=1, title="Data Input", description=""),
+    stx.TabBarItemData(id=2, title="Results", description=""),
+], default=1)
 
-with col1:
-    latitude = st.number_input('Latitude', value=56)
-with col2:
-    longitude = st.number_input('Longitude', value=-2)
-with col3:
-    accumulated_temperature = st.number_input('Accumulated Temperature', value=1400)
-with col4:
-    soil_moisture_regime = st.number_input('SMR', value=3)
-with col5:
-    soil_nutrient_regime = st.number_input('SNR', value=3)
+if str(chosen_id) == '1':
 
+    col1, col2, col3, col4, col5 =  st.columns(5)
 
-with open('model.pickle', 'rb') as file:
-    model = pickle.load(file)
+    with col1:
+        latitude = st.number_input('Latitude', value=56)
+        st.session_state['latitude'] = latitude
+    with col2:
+        longitude = st.number_input('Longitude', value=-2)
+        st.session_state['longitude'] = longitude
+    with col3:
+        accumulated_temperature = st.number_input('Accumulated Temp', value=1400, step=10)
+        st.session_state['accumulated_temperature'] = accumulated_temperature
+    with col4:
+        soil_moisture_regime = st.number_input('SMR', value=3)
+        st.session_state['soil_moisture_regime'] = soil_moisture_regime
+    with col5:
+        soil_nutrient_regime = st.number_input('SNR', value=3)
+        st.session_state['soil_nutrient_regime'] = soil_nutrient_regime
 
-
-    input_data = pd.DataFrame({
-        'latitude': [latitude],
-        'longitude': [longitude],
-        'accumulated_temperature': [accumulated_temperature],
-        'soil_moisture_regime': [soil_moisture_regime],
-        'soil_nutrient_regime': [soil_nutrient_regime]})
-
+if str(chosen_id) == '2':
     with st.spinner('Running model...'):
-        prediction = model.predict(input_data)
-        prediction_df = pd.DataFrame(prediction, columns=['AH', 'ASP', 'BPO', 'CAR', 'HBM', 'PBI', 'POK', 'ROK', 'ROW', 'SBI', 'SC', 'SLI', 'SOK', 'SP', 'WCH', 'WEM'])
+        with open('model.pickle', 'rb') as file:
 
-        top_species = prediction_df.T.nlargest(3, 0).reset_index()
-        top_species.columns = ['Species', 'Ecosuit Score']
+            model = pickle.load(file)
+            st.session_state['model'] = model
 
-        st.dataframe(top_species)
 
+            input_data = pd.DataFrame({
+                'latitude': [st.session_state.latitude],
+                'longitude': [st.session_state.longitude],
+                'accumulated_temperature': [st.session_state.accumulated_temperature],
+                'soil_moisture_regime': [st.session_state.soil_moisture_regime],
+                'soil_nutrient_regime': [st.session_state.soil_nutrient_regime]})
+
+            st.session_state['input_data'] = input_data
+
+            file.close()
+
+            prediction = st.session_state.model.predict(st.session_state.input_data)
+            prediction_df = pd.DataFrame(prediction, columns=['AH', 'ASP', 'BPO', 'CAR', 'HBM', 'PBI', 'POK', 'ROK', 'ROW', 'SBI', 'SC', 'SLI', 'SOK', 'SP', 'WCH', 'WEM'])
+
+            top_species = prediction_df.T.nlargest(5, 0).reset_index()
+            top_species.columns = ['Species', 'Ecosuit Score']
+
+            st.dataframe(top_species, use_container_width=True)
+
+st.markdown('---')
+feedback = st.feedback("faces")
+if feedback:
+    st.write('Thank you for submitting feedback')
