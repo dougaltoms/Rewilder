db3cbe453d2246a7598a1a0226bbc328ebf7d953
dougaltoms <dougal.toms@gmail.com>
2024-09-16 21:52:23 +0100

    General stuff

--- app.py
+++ app.py
@@ -8,41 +8,46 @@ import folium
 from folium import Marker
 import geopandas as gpd
 from shapely.geometry import Point
+from utils.utils import return_features
 
 st.header('Rewilder')
 st.subheader('Mapping rewilding suitability for native ancient tree species')
 
-df = pd.read_csv('/Users/dougaltoms/Documents/Rewilder/ecological_site_classification.csv')
+df = pd.read_csv('ecological_site_classification.csv')
+features_df = pd.read_csv('esc_features.csv')
 
-chosen_id = stx.tab_bar(data=[
-    stx.TabBarItemData(id=1, title="Data Input", description=""),
-    stx.TabBarItemData(id=2, title="Results", description=""),
-    stx.TabBarItemData(id=3, title="Map", description=""),
-    ], default=1)
+# chosen_id = stx.tab_bar(data=[
+#     stx.TabBarItemData(id=1, title="Data Input", description=""),
+#     stx.TabBarItemData(id=2, title="Results", description=""),
+#     stx.TabBarItemData(id=3, title="Map", description=""),
+#     ], default=1)
 
-if str(chosen_id) == '1':
+tab1, tab2, tab3 = st.tabs(['Data Input', 'Results', 'Mapping'])
 
-    col1, col2, col3, col4, col5 =  st.columns(5)
+# if str(chosen_id) == '1':
+with tab1:
+    col1, col2 =  st.columns(2)
     with col1:
-        latitude = st.number_input('Latitude', value=56, min_value = 50, max_value=60)
+        latitude = st.number_input('Latitude', value=54.0, min_value = 50.0, max_value=60.0, step=.1)
         st.session_state['latitude'] = latitude
     with col2:
-        longitude = st.number_input('Longitude', value=-2, min_value=-7, max_value=2)
+        longitude = st.number_input('Longitude', value=-3.0, min_value=-7.0, max_value=2.0, step=.1)
         st.session_state['longitude'] = longitude
-    with col3:
-        accumulated_temperature = st.number_input('Accumulated Temp', value=1400, step=10, min_value = 140, max_value=2200)
-        st.session_state['accumulated_temperature'] = accumulated_temperature
-    with col4:
-        soil_moisture_regime = st.selectbox('SMR', options=[0,1,2,3,4,5,6,7,8], index=4)
-        st.session_state['soil_moisture_regime'] = soil_moisture_regime
-    with col5:
-        soil_nutrient_regime = st.selectbox('SNR', options=[0,1,2,3,4,5,6],index=3)
-        st.session_state['soil_nutrient_regime'] = soil_nutrient_regime
 
-    with st.expander('Show map'):
-        st.image('uk_agri_capability.png')
+    accumulated_temperature, soil_moisture_regime, soil_nutrient_regime = return_features(features_df, st.session_state.latitude, st.session_state.longitude)
+    st.session_state['accumulated_temperature'] = accumulated_temperature
+    st.session_state['soil_moisture_regime'] = soil_moisture_regime
+    st.session_state['soil_nutrient_regime'] = soil_nutrient_regime
 
-if str(chosen_id) == '2':
+    res1, res2, res3 =st.columns(3)
+    with res1:
+        st.metric('Accumulated Temperature', accumulated_temperature)
+    with res2:  
+        st.metric('Soil Moisture Regime', soil_moisture_regime)
+    with res3:
+        st.metric('Soil Nutrient Regime', soil_nutrient_regime)
+
+with tab2:
     with st.spinner('Running model...'):
         with open('pickle/model.pickle', 'rb') as file:
 
@@ -67,43 +72,56 @@ if str(chosen_id) == '2':
             species_df = prediction_df.T.nlargest(5, 0).reset_index()
             species_df.columns = ['Species Code', 'Ecosuit Score']
             species_df['Species'] = species_df['Species Code'].map(tree_mapping)
-
     
             st.dataframe(species_df[['Species','Species Code', 'Ecosuit Score']], use_container_width=True, hide_index=True)
 
-if str(chosen_id) == '3':
+with tab3:
 
-    # st.write('Streamlit Plot')
-    # df = df[df['latitude'].between((st.session_state.latitude)-.5, (st.session_state.latitude)+.5)]
-    # df = df[df['longitude'].between((st.session_state.longitude)-.5, (st.session_state.longitude)+.5)]
-    # st.map(df)
+    gpd_plot = st.toggle('Folium Plot')
 
-    filtered_plot = st.toggle('Plot Land Use')
-    if filtered_plot:
-        with st.spinner('Creating Map'):
-            with open('pickle/uk_ag.pickle', 'rb') as file:
-                    
-                    uk_ag = pickle.load(file)
-                    st.session_state['uk_ag'] = uk_ag[['geometry', 'NUMERIC_GRADE']]
+    popup_text = f''' <strong>Top 3 Predicted Tree Species</strong><br>
+                            1. {species_df['Species'].iloc[0]} ({round(species_df['Ecosuit Score'].iloc[0],2)})<br>
+                            2. {species_df['Species'].iloc[1]} ({round(species_df['Ecosuit Score'].iloc[1],2)})<br>
+                            3. {species_df['Species'].iloc[2]} ({round(species_df['Ecosuit Score'].iloc[2],2)})'''
+    
+    with st.spinner('Creating Map'):
+        with open('pickle/uk_ag.pickle', 'rb') as file:
                 
-            point = gpd.GeoSeries([Point(st.session_state.longitude, st.session_state.latitude)], crs="EPSG:4326")
+                uk_ag = pickle.load(file)
+                st.session_state['uk_ag'] = uk_ag[['geometry', 'NUMERIC_GRADE']]
+            
+        point = gpd.GeoSeries([Point(st.session_state.longitude, st.session_state.latitude)], crs="EPSG:4326")
 
-            if st.session_state.uk_ag.crs != point.crs:
-                st.session_state.uk_ag = st.session_state.uk_ag.to_crs(point.crs)
+        if st.session_state.uk_ag.crs != point.crs:
+            st.session_state.uk_ag = st.session_state.uk_ag.to_crs(point.crs)
 
-            st.session_state.uk_ag['distance_to_point'] = st.session_state.uk_ag['geometry'].distance(point[0])
-            threshold = .2
-            uk_ag_filtered = st.session_state.uk_ag[st.session_state.uk_ag['distance_to_point'] <= threshold]
-            uk_ag_filtered = uk_ag_filtered.dissolve(by='NUMERIC_GRADE')
-            uk_ag_filtered = uk_ag_filtered.reset_index()
+        st.session_state.uk_ag['distance_to_point'] = st.session_state.uk_ag['geometry'].distance(point[0])
+        threshold = .2
+        uk_ag_filtered = st.session_state.uk_ag[st.session_state.uk_ag['distance_to_point'] <= threshold]
+        uk_ag_filtered = uk_ag_filtered.dissolve(by='NUMERIC_GRADE')
+        uk_ag_filtered = uk_ag_filtered.reset_index()
+        uk_ag_filtered = uk_ag_filtered[['geometry', 'NUMERIC_GRADE']]
+        
+        if gpd_plot:
 
+            st.write('Geopandas `explore()` functionality')
             m = uk_ag_filtered.explore('NUMERIC_GRADE',cmap='Blues',tiles="Stadia.AlidadeSmoothDark", attr = "© Dougal Toms")
-            # m = folium.Map(zoom_start=8, location=[st.session_state.latitude, st.session_state.longitude], tiles="Esri.WorldImagery", attr = "© Dougal Toms")
-            # choropleth = folium.Choropleth(uk_ag_filtered, columns=['NUMERIC_GRADE'], fill_color='Blues', name='Land grade', highlight=True).add_to(m)
-            # folium.LayerControl(collapsed=False).add_to(m)
-            st_folium(m)
+            folium.Marker(location=[st.session_state.latitude, st.session_state.longitude],popup=folium.Popup(popup_text, max_width=300), icon=folium.Icon(color="green", icon="tree")).add_to(m)
+            st_folium(m,width=1000, height=500)
+
+        else:
+
+            st.write('Native `folium` functionality')
+            m = folium.Map(zoom_start=8, location=[st.session_state.latitude, st.session_state.longitude],tiles="Stadia.AlidadeSmoothDark", attr = "© Dougal Toms")
+            folium.Choropleth(geo_data=uk_ag_filtered, columns=['geometry','NUMERIC_GRADE'], fill_color='YlGn').add_to(m)
+            folium.Marker(location=[st.session_state.latitude, st.session_state.longitude],popup=folium.Popup(popup_text, max_width=300), icon=folium.Icon(color="green", icon="tree")).add_to(m)
+            # folium.Marker(location=[st.session_state.latitude, st.session_state.longitude],popup='User input',).add_to(m)
+            st_folium(m,width=1000, height=500)
+
 
 st.markdown('---')
+with st.expander('Show map'):
+    st.image('uk_agri_capability.png')
 feedback = st.feedback("faces")
 if feedback:
     st.write('Thank you for submitting feedback')
